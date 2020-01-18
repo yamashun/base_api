41e32f1e148e7d67ef9dfa8e3be5a30939efe978
yamashun <yshunske@gmail.com>
2020-01-18 18:10:26 +0900

    add fetch items spec

---
+++ spec/client/items_spec.rb
@@ -0,0 +1,25 @@
+RSpec.describe BaseApi::Client::Items do
+  describe '.items', :vcr do
+    before do
+      BaseApi.reset!
+    end
+
+    let(:client) { BaseApi::Client.new(access_token: test_access_token) }
+
+    it 'returns items' do
+      items = client.items
+      expect(items['items'].first['item_id']).not_to be_nil
+      assert_requested :get, base_api_url("/1/items?offset=0&limit=10")
+    end
+
+    it 'returns items and returns next page' do
+      items = client.items(limit: 1)
+      expect(items['items'].first['item_id']).not_to be_nil
+      assert_requested :get, base_api_url("/1/items?offset=0&limit=1")
+
+      client.fetch_next_page
+      expect(items['items'].first['item_id']).not_to be_nil
+      assert_requested :get, base_api_url("/1/items?offset=0&limit=1")
+    end
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require "bundler/setup"
 require "base_api"
 require "pry"
+require 'webmock/rspec'
+require 'vcr'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -12,4 +14,25 @@ RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
+end
+
+VCR.configure do |c|
+  c.configure_rspec_metadata!
+  c.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  c.hook_into :webmock
+  c.filter_sensitive_data('<BearerToken>') { test_access_token }
+end
+
+
+def base_api_url(url)
+  return url if url =~ /^http/
+
+  url = File.join('https://api.thebase.in', url)
+  uri = Addressable::URI.parse(url)
+
+  uri.to_s
+end
+
+def test_access_token
+  ENV.fetch('BASE_API_ACCESS_TOKEN'){ 'hogehogeaccesstoken' }
 end
