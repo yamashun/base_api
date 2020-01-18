9c6ddc581d4ceb1b56725446fa620d8558eda8b6
yamashun <yshunske@gmail.com>
2020-01-18 11:54:24 +0900

    feat: set default values

--- lib/base_api/default.rb
+++ lib/base_api/default.rb
@@ -11,35 +11,35 @@ module BaseApi
 
       # TODO: set default values
       def client_id
-        ""
+        ENV['BASE_API_CLIENT_ID']
       end
 
       def client_secret
-        ""
+        ENV['BASE_API_CLIENT_SECRET']
       end
 
       def code
-        ""
+        ENV['BASE_API_CODE']
       end
 
       def access_token
-        ""
+        ENV['BASE_API_ACCESS_TOKEN']
       end
 
       def refresh_token
-        ""
+        ENV['BASE_API_REFRESH_TOKEN']
       end
 
       def per_page
-        ""
+        ENV.fetch("BASE_API_PER_PAGE") { 10 }
       end
 
       def offset
-        ""
+        ENV.fetch("BASE_API_OFFSET") { 0 }
       end
 
       def redirect_uri
-        ""
+        ENV['BASE_API_REDIRECT_URI']
       end
     end
   end
