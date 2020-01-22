51422675765e43e3233e170d1f0b2d59798c891c
yamashun <yshunske@gmail.com>
2020-01-22 23:44:30 +0900

    add items search

--- lib/base_api/client/items.rb
+++ lib/base_api/client/items.rb
@@ -1,8 +1,20 @@
 module BaseApi
   class Client
     module Items
+      # @option params [Hash] option
+      # @return [HTTParty::Response]
+      # @see https://docs.thebase.in/docs/api/items/
       def items(option = {})
         paginate('/1/items', option)
+      end
+
+
+      # @param [String] q: search word
+      # @option params [Hash] option
+      # @return [HTTParty::Response]
+      # @see https://docs.thebase.in/docs/api/items/search
+      def items_search(q, option = {})
+        paginate('/1/items/search', { q: q }.merge(option))
       end
     end
   end
--- spec/client/items_spec.rb
+++ spec/client/items_spec.rb
@@ -22,4 +22,31 @@ RSpec.describe BaseApi::Client::Items do
       assert_requested :get, base_api_url("/1/items?offset=1&limit=1")
     end
   end
+
+  describe '.items_search', :vcr do
+    before do
+      BaseApi.reset!
+    end
+
+    let(:client) { BaseApi::Client.new(access_token: test_base_access_token) }
+
+    context 'without option' do
+      let(:serach_word) { 'キングダム' }
+      it 'returns items' do
+        reponse = client.items_search(serach_word)
+        expect(reponse['items'].first['item_id']).not_to be_nil
+        assert_requested :get, base_api_url("/1/items/search?q=#{CGI.escape(serach_word)}&offset=0&limit=10")
+      end
+    end
+
+    context 'with option' do
+      let(:serach_word) { 'キングダム' }
+      let(:option){ { fields: 'title,detail' } }
+      it 'returns items' do
+        reponse = client.items_search(serach_word, option)
+        expect(reponse['items'].first['item_id']).not_to be_nil
+        assert_requested :get, base_api_url("/1/items/search?q=#{CGI.escape(serach_word)}&offset=0&limit=10&fields=title,detail")
+      end
+    end
+  end
 end
