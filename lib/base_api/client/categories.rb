023291fda65aa6a30fb9ae9af73b567f320d4832
yamashun <yshunske@gmail.com>
2020-02-01 21:47:04 +0900

    add categories/add

--- lib/base_api/client/categories.rb
+++ lib/base_api/client/categories.rb
@@ -6,6 +6,14 @@ module BaseApi
       def categories
         get_call_api('/1/categories')
       end
+
+      # @param [String] name
+      # @option param [Hash] option
+      # @return [HTTParty::Response]
+      # @see https://docs.thebase.in/docs/api/categories/
+      def categories_add(name, option = {})
+        post_call_api('/1/categories/add',  { name: name }.merge(option))
+      end
     end
   end
 end
--- spec/client/categories_spec.rb
+++ spec/client/categories_spec.rb
@@ -3,13 +3,28 @@ RSpec.describe BaseApi::Client::Categories do
     before do
       BaseApi.reset!
     end
-
     let(:client) { BaseApi::Client.new(access_token: test_base_access_token) }
 
     it 'returns categories' do
       reponse = client.categories
       expect(reponse['categories']).not_to be_nil
       assert_requested :get, base_api_url('/1/categories')
+    end
+  end
+
+  describe '.categories_add', :vcr do
+    before do
+      BaseApi.reset!
+    end
+    let(:client) { BaseApi::Client.new(access_token: test_base_access_token) }
+    let(:name) { 'category2' }
+    let(:option) { { list_order: 2, parent_number: 1 } }
+
+    it 'adds a category' do
+      reponse = client.categories_add(name, option)
+      expect(reponse['categories']).not_to be_nil
+      assert_requested :post, base_api_url('/1/categories/add'),
+        body: URI.encode_www_form({ name: name}.merge(option))
     end
   end
 end
